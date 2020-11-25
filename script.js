c32adbdefa96a12aa034458e85b25047b84e2e68
Josep <aliverock@icloud.com>
2020-11-25 20:07:07 +0100

    the first commit of the first Portfolio project

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log('this is a test');
-console.log('this is still a test');
+
+function randomMessage (name) {
+    
+    const arrayOne = ['eternally', 'always', 'forever'];
+    const arrayTwo = ['keep', 'be', 'stay'];
+    const arrayThree = ['hungry', 'happy', 'young'];
+    
+    let arrayOneRand = arrayOne[Math.floor(Math.random()*3)];
+    let arrayTwoRand = arrayTwo[Math.floor(Math.random()*3)];
+    let arrayThreeRand = arrayThree[Math.floor(Math.random()*3)];
+    console.log(`Hey, ${name}: ${arrayOneRand} ${arrayTwoRand} ${arrayThreeRand}!`);
+
+}
+randomMessage('John');
