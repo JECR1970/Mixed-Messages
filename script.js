a7900cfd44486076405b38ee7f078c6ca2175a03
Josep <aliverock@icloud.com>
2020-11-26 18:46:17 +0100

    version of Nov 26th

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
-function randomMessage (name) {
+function randomMessage () {
     
     const arrayOne = ['eternally', 'always', 'forever'];
     const arrayTwo = ['keep', 'be', 'stay'];
-    const arrayThree = ['hungry', 'happy', 'young'];
+    const arrayThree = ['hungry', 'foolish', 'young'];
     
     let arrayOneRand = arrayOne[Math.floor(Math.random()*3)];
     let arrayTwoRand = arrayTwo[Math.floor(Math.random()*3)];
     let arrayThreeRand = arrayThree[Math.floor(Math.random()*3)];
-    console.log(`Hey, ${name}: ${arrayOneRand} ${arrayTwoRand} ${arrayThreeRand}!`);
-
+    
+        console.log(`Hey: ${arrayOneRand} ${arrayTwoRand} ${arrayThreeRand}!`);
 }
-randomMessage('John');
+randomMessage();
